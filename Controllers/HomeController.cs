36d7e88ab56430e0ffce6c154a349b45ad8fbc35
Karuizawa Kei <113401107+KeiKaruizawa@users.noreply.github.com>
2026-07-16 14:24:01 +0800

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-using System.Diagnostics;
 using BolascoInfass.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BolascoInfass.Controllers
 {
@@ -23,58 +23,23 @@ namespace BolascoInfass.Controllers
             return View();
         }
 
-        [HttpGet("/Login")]
-        public IActionResult Login()
-        {
-            return View();
-        }
-
-        [HttpPost("/Login")]
-        public IActionResult Login(LoginViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            bool isValidUser = false;
-
-            if (isValidUser)
-            {
-                return RedirectToAction("Index", "Home");
-            }
-
-            ModelState.AddModelError(string.Empty, "Invalid email or password.");
-            return View(model);
-        }
-
-        [HttpGet("/Register")]
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        [HttpPost("/Register")]
-        public IActionResult Register(RegisterViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-
-            if (model.Password != model.ConfirmPassword)
-            {
-                ModelState.AddModelError("ConfirmPassword", "Passwords do not match.");
-                return View(model);
-            }
-
-            return RedirectToAction("Login");
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [Route("Register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
